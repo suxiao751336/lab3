52724309f6e97fa9366befa4a119f8e88723c419
suxiao751336 <suxiao20152015@163.com>
2019-10-11 12:05:16 +0200

    as

--- server.js
+++ server.js
@@ -3,31 +3,47 @@ var mysql = require('mysql');
 var app = express();
 
 
-var connection =mysql.createConnection({
-	
-	host: 'us-cdbr-iron-east-05.cleardb.net',
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host: 'us-cdbr-iron-east-05.cleardb.net',
 	user: 'ba7c7911ddf522',
 	password: '4847e5c8',
 	database: 'heroku_9d1a16551a2b098'
-});
-
-connection.connect();
-
-
-
-
-
-app.get('/', function(request,response){
-	 connection.query('select * from suxiaodb',function(err,rows,fields){
-	    if(err){
+  });
+  app.get('/', function(request,response){
+  pool.query('select * from suxiaodb', function (err, rows, fields) {
+    if(err){
 		console.log('error',err);
 		throw err;
 	}
 		response.send(['hello Mr xiao!!!',rows]);
+  });
+});
+// var connection =mysql.createConnection({
+	
+// 	host: 'us-cdbr-iron-east-05.cleardb.net',
+// 	user: 'ba7c7911ddf522',
+// 	password: '4847e5c8',
+// 	database: 'heroku_9d1a16551a2b098'
+// });
+
+// connection.connect();
+
+
+
+
+
+// app.get('/', function(request,response){
+// 	 connection.query('select * from suxiaodb',function(err,rows,fields){
+// 	    if(err){
+// 		console.log('error',err);
+// 		throw err;
+// 	}
+// 		response.send(['hello Mr xiao!!!',rows]);
 		
 	
-});
-});
+// });
+// });
 var port =process.env.PORT || 5000;
 
 app.listen(port,function(){
